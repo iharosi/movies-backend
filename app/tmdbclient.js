dcc7e8a4284e6d3ba68f9644b54574a7b8fd437a
Gergely Iharosi <iharosi@gmail.com>
2016-10-04 15:21:08 +0200

    Implemented config function
    * session_id variable can be set or the expiration time can be changed, etc

--- app/tmdbclient.js
+++ app/tmdbclient.js
@@ -1,27 +1,32 @@
 /* eslint camelcase: ["error", {properties: "never"}] */
 
-let TMDbClient = function(APIkey) {
-    if (!APIkey || APIkey.length !== 32) {
+let TMDbClient = function(key) {
+    if (!key || key.length !== 32) {
         throw new Error(
             'Invalid API key: You must be granted a valid key.'
         );
     }
 
-    const BASE_PATH = 'https://api.themoviedb.org/3';
-    const CONFIG_EXPIRATION = 72; // hours
     const request = require('request');
 
     let configuration = null;
     let lastConfigDate = Date.now();
 
+    this.key = key;
+    this.basePath = 'https://api.themoviedb.org/3';
+    this.configExpiration = 72; // hours
+
+    /**
+     * @return {Promise} Get API configuration information.
+     */
     this._fetchConfiguration = function() {
         return new Promise((resolve, reject) => {
             // TMDb suggests to cache this data and check for updates
             // every few days so a new config will be requested when the
-            // current config older than 3 days
+            // current config older than what have been set in configExpiration
             let ageOfConfig = Math
                 .round((Date.now() - lastConfigDate) / 1000 / 60 / 60);
-            if (configuration === null || ageOfConfig > CONFIG_EXPIRATION) {
+            if (configuration === null || ageOfConfig > this.configExpiration) {
                 this.call('/configuration', {}).then((res) => {
                     lastConfigDate = Date.now();
                     resolve(res);
@@ -34,22 +39,56 @@ let TMDbClient = function(APIkey) {
         });
     };
 
-    this._createRequestOption = function(url, params) {
+    /**
+     * @param {string} url API endpoint path (required)
+     * @param {object} params Query string parameters (optional)
+     * @param {string} method HTTP method (optional, default is: GET)
+     * @param {object} body Request body (optional)
+     *
+     * @return {object} The final request options
+     */
+    this._createRequestOption = function(url, params, method, body) {
+        params = params || {};
+        method = method || 'GET';
+        body = body || {};
+        let queryString = Object.assign(params, {api_key: this.key});
+        if (this.sessionId) {
+            Object.assign(queryString, {session_id: this.sessionId});
+        }
         return {
-            method: 'GET',
-            url: BASE_PATH + url,
-            qs: Object.assign(params, {api_key: APIkey}),
+            method: method,
+            url: this.basePath + url,
+            qs: queryString,
             headers: {
                 'content-type': 'application/json'
             },
-            body: {},
+            body: body,
             json: true
         };
     };
 
-    this.call = function(url, params) {
+    /**
+     * @param {object} options Change variables or set session_id
+     */
+    this.config = function(options) {
+        if (typeof options === 'object') {
+            Object.getOwnPropertyNames(options).forEach((property) => {
+                this[property] = options[property];
+            });
+        }
+    };
+
+    /**
+     * @param {string} url API endpoint path (required)
+     * @param {object} params Query string parameters (optional)
+     * @param {string} method HTTP method (optional, default is: GET)
+     * @param {object} body Request body (optional)
+     *
+     * @return {Promise} Request response body
+     */
+    this.call = function(url, params, method, body) {
         return new Promise((resolve, reject) => {
-            let options = this._createRequestOption(url, params);
+            let options = this._createRequestOption(url, params, method, body);
             request(options, (error, response, body) => {
                 if (error) {
                     reject(new Error(error));
