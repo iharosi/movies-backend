afac4fe34ddcea4e2847f3cb0fbe83d1d855f9c0
Gergely Iharosi <iharosi@gmail.com>
2016-10-12 15:21:25 +0200

    Close #4 – Watch directory changes
    Introduced directory change events handling with Chokidar.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* eslint camelcase: ["error", {properties: "never"}] */
 
-const fs = require('fs');
 const co = require('co');
 const path = require('path');
+const chokidar = require('chokidar');
 const Bottleneck = require('bottleneck');
 const TMDbClient = require('./lib/tmdbclient');
 const TMDbAuth = require('./lib/tmdbauth');
@@ -14,7 +14,7 @@ let tmdb = new TMDbClient(config.tmdb.key);
 let tmdba = new TMDbAuth(config.tmdb.key);
 let cache = new Cacher(path.join(__dirname, './session'));
 
-co(function* () {
+co(function* init() {
     let sessionId;
     let account;
     sessionId = yield cache.getData();
@@ -34,29 +34,41 @@ co(function* () {
     }
     for (let i = 0; i < config.tmdb.lists.length; i += 1) {
         let list = config.tmdb.lists[i];
-        let movieDatas = yield extractDataFromFolderNames(
-            path.join(__dirname, list.folder)
-        );
-        let movieMetadatas = yield findMovieMetadatas(movieDatas);
-        let localMovies = movieMetadatas.map((metadata, i) => {
-            let result = {
-                _source: movieDatas[i]
-            };
-            if (metadata.total_results > 0) {
-                Object.assign(result, metadata.results[0]);
-            } else {
-                console.log(result);
-            }
-            return result;
-        });
-        let tmdbMovies = yield tmdb.call(`/list/${list.id}`, {
+        let dir = path.join(__dirname, list.folder);
+
+        list.movies = yield tmdb.call(`/list/${list.id}`, {
             session_id: sessionId
         });
-        let changes = getDiff(localMovies, tmdbMovies.items);
 
-        yield addOrRemoveMovies(changes, list.id, sessionId);
+        let watcher = chokidar.watch(dir, {
+            persistent: true,
+            ignored: /[\/\\]\./,
+            ignoreInitial: false,
+            followSymlinks: false,
+            cwd: dir,
+            depth: 0
+        });
 
-        logSummary(changes);
+        watcher
+            .on('addDir', (folder) => {
+                if (folder) {
+                    console.log(`+ \x1b[32m${folder}\x1b[0m`);
+                    addMovie(folder, list, sessionId);
+                }
+            })
+            .on('unlinkDir', (folder) => {
+                if (folder) {
+                    console.log(`- \x1b[35m${folder}\x1b[0m`);
+                    removeMovie(folder, list, sessionId);
+                }
+            })
+            .on('error', (error) => {
+                console.log(`\x1b[31mError\x1b[0m`, error);
+            })
+            .on('ready', () => {
+                console.log(`\x1b[2mInitial scan complete. ` +
+                    `Ready for changes.\x1b[0m`);
+            });
     }
 }).catch((error) => {
     console.log('Error', error);
@@ -64,144 +76,197 @@ co(function* () {
 });
 
 /**
- * @param {Object} changes The list of removed and added movies
+ * @param {String} folder Folder name
+ * @param {Object} list List item from config
+ * @param {Number} sessionID Session ID
  */
-function logSummary(changes) {
-    changes.remove = changes.remove.map((movie) => {
-        return movie.title;
-    });
-    changes.add = changes.add.map((movie) => {
-        return movie.title;
-    });
-    if (changes.remove.length) {
-        console.log('––– removed –––');
-        console.log(changes.remove.join('\n'));
-        console.log();
-    }
-    if (changes.add.length) {
-        console.log('––– added –––');
-        console.log(changes.add.join('\n'));
-        console.log();
-    }
-}
-
-/**
- * @param {Array.<object>} localMovies Metadatas of local movies
- * @param {Array.<object>} tmdbMovies Metadatas of cloud movies
- *
- * @return {Object} Should be deleted and should be added lists of movies
- */
-function getDiff(localMovies, tmdbMovies) {
-    return {
-        remove: tmdbMovies.filter((tmdbMovie) => {
-            return !localMovies.find((localMovie) => {
-                return localMovie.id === tmdbMovie.id;
+function removeMovie(folder, list, sessionID) {
+    let movie = extractDataFromFolderName(folder);
+    if (movie && movie.title && movie.year && movie.folder) {
+        findMovieMetadata(movie)
+            .then((metadata) => {
+                let id = null;
+                if (metadata.total_results > 0) {
+                    id = metadata.results[0].id;
+                } else {
+                    console.log(`\x1b[31mCouldn't locate movie in TMDb:`,
+                    `${movie.title} (${movie.year}) – ${movie.folder}\x1b[0m`);
+                }
+                return id;
+            })
+            // TODO: Introduce a Store where
+            // remote and local movies can be maintained
+            //
+            // .then((movieID) => {
+            //     let found = list.movies.items.find((item) => {
+            //         return item.id === movieID;
+            //     });
+            //     return found ? movieID : null;
+            // })
+            .then((movieID) => {
+                let result = null;
+                if (movieID) {
+                    result = removeMovieFromList(
+                        movieID,
+                        list.id,
+                        sessionID
+                    );
+                }
+                return result;
+            })
+            .then((result) => {
+                if (result && result.status_code !== 8) {
+                    console.log(
+                        `\x1b[33m${result.status_message} ` +
+                        `(${result.status_code})\x1b[0m`
+                    );
+                }
+            })
+            .catch((e) => {
+                console.log(e);
             });
-        }),
-        add: localMovies.filter((localMovie) => {
-            return !tmdbMovies.find((tmdbMovie) => {
-                return tmdbMovie.id === localMovie.id;
-            });
-        })
-    };
+    } else {
+        console.log(
+            `\x1b[31mMovie title could not extract from: \x1b[0m`,
+            folder
+        );
+    }
 }
 
 /**
- * @param {Object} movies The list of movies what should be added and/or removed
- * @param {Number} listId The ID of the selected List
- * @param {Number} sessionId Session ID
- *
- * @return {Array.<promise>} An array of API call response bodies
+ * @param {String} folder Folder name
+ * @param {Object} list List item from config
+ * @param {Number} sessionID Session ID
  */
-function addOrRemoveMovies(movies, listId, sessionId) {
-    return Promise.all(
-        movies.remove.map((movie) => {
-            return limiter.schedule(
-                tmdb.call.bind(
-                    tmdb,
-                    `/list/${listId}/remove_item`,
-                    {
-                        session_id: sessionId
-                    },
-                    'POST',
-                    {
-                        media_id: movie.id
-                    }
-                )
-            );
-        }).concat(movies.add.map((movie) => {
-            return limiter.schedule(
-                tmdb.call.bind(
-                    tmdb,
-                    `/list/${listId}/add_item`,
-                    {
-                        session_id: sessionId
-                    },
-                    'POST',
-                    {
-                        media_id: movie.id
-                    }
-                )
-            );
-        }))
-    );
+function addMovie(folder, list, sessionID) {
+    let movie = extractDataFromFolderName(folder);
+    if (movie && movie.title && movie.year && movie.folder) {
+        findMovieMetadata(movie)
+            .then((metadata) => {
+                let movieID = null;
+                if (metadata.total_results > 0) {
+                    movieID = metadata.results[0].id;
+                } else {
+                    console.log(`\x1b[31mCouldn't locate movie in TMDb:`,
+                    `${movie.title} (${movie.year}) – ${movie.folder}\x1b[0m`);
+                }
+                return movieID;
+            })
+            // TODO: Introduce a Store where
+            // remote and local movies can be maintained
+            //
+            // .then((movieID) => {
+            //     let found = list.movies.items.find((item) => {
+            //         return item.id === movieID;
+            //     });
+            //     return found ? null : movieID;
+            // })
+            .then((movieID) => {
+                let result = null;
+                if (movieID) {
+                    result = addMovieToList(
+                        movieID,
+                        list.id,
+                        sessionID
+                    );
+                }
+                return result;
+            })
+            .then((result) => {
+                if (result && result.status_code !== 8) {
+                    console.log(
+                        `\x1b[33m${result.status_message} ` +
+                        `(${result.status_code})\x1b[0m`
+                    );
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    } else {
+        console.log(
+            `\x1b[31mMovie title could not extract from: \x1b[0m`,
+            folder
+        );
+    }
 }
 
 /**
- * @param {Array.<object>} movies Extracted data from folder names
+ * @param {Number} movieID The ID of the movie
+ * @param {Number} listID The ID of the list
+ * @param {Number} sessionID Session ID
  *
  * @return {Promise} API call response body
  */
-function findMovieMetadatas(movies) {
-    return Promise.all(
-        movies.map((movie) => {
-            return limiter.schedule(
-                tmdb.call.bind(
-                    tmdb,
-                    '/search/movie',
-                    {
-                        query: movie.title,
-                        year: movie.year
-                    }
-                )
-            );
-        })
+function removeMovieFromList(movieID, listID, sessionID) {
+    return limiter.schedule(
+        tmdb.call.bind(
+            tmdb,
+            `/list/${listID}/remove_item`,
+            {
+                session_id: sessionID
+            },
+            'POST',
+            {
+                media_id: movieID
+            }
+        )
     );
 }
 
 /**
- * @param {string} sourceDir Where the movie folders can be found
+ * @param {Number} movieID The ID of the movie
+ * @param {Number} listID The ID of the list
+ * @param {Number} sessionID Session ID
  *
- * @return {Promise} Array contains objects of movie data (title/year)
+ * @return {Promise} API call response body
  */
-function extractDataFromFolderNames(sourceDir) {
-    return new Promise((resolve, reject) => {
-        fs.readdir(sourceDir, (err, items) => {
-            if (err) {
-                reject(new Error(err));
+function addMovieToList(movieID, listID, sessionID) {
+    return limiter.schedule(
+        tmdb.call.bind(
+            tmdb,
+            `/list/${listID}/add_item`,
+            {
+                session_id: sessionID
+            },
+            'POST',
+            {
+                media_id: movieID
             }
-            if (items && items.length) {
-                let data = items
-                    .filter((item) => {
-                        return fs
-                            .statSync(path.join(sourceDir, item))
-                            .isDirectory();
-                    })
-                    .filter((item) => {
-                        return item.match(/^(.+)\.(\d{4})\./i) !== null;
-                    })
-                    .map((item) => {
-                        let matched = item.match(/^(.+)\.(\d{4})\./i);
-                        return {
-                            title: matched[1].replace(/\./g, ' '),
-                            year: matched[2],
-                            folder: item
-                        };
-                    });
-                resolve(data);
-            } else {
-                resolve([]);
+        )
+    );
+}
+
+/**
+ * @param {Object} movie Extracted data from folder name
+ *
+ * @return {Promise} API call response body
+ */
+function findMovieMetadata(movie) {
+    return limiter.schedule(
+        tmdb.call.bind(
+            tmdb,
+            '/search/movie',
+            {
+                query: movie.title,
+                year: movie.year
             }
-        });
-    });
+        )
+    );
+}
+
+/**
+ * @param {String} folderName Folder name what contains movie title and year
+ *
+ * @return {Object} Movie data (title, year) and folder name
+ */
+function extractDataFromFolderName(folderName) {
+    let matched = folderName.match(/^(.+)\.(\d{4})\./i);
+    if (matched && matched.length >= 2) {
+        return {
+            title: matched[1].replace(/\./g, ' '),
+            year: matched[2],
+            folder: folderName
+        };
+    }
 }
